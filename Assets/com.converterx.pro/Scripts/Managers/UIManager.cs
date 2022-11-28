ff975a8ed21944d25377b74c9790a7d73090d254
Sladkiy Eduard <nikcxprojects@gmail.com>
2022-11-28 17:24:20 +0300

    update

--- Assets/com.converterx.pro/Scripts/Managers/UIManager.cs
+++ Assets/com.converterx.pro/Scripts/Managers/UIManager.cs
@@ -6,25 +6,19 @@ public class UIManager : MonoBehaviour
 {
     private GameObject _last = null;
 
+    [SerializeField] GameObject navigation;
+
+    [Space(10)]
+    [SerializeField] GameObject getStarted;
     [SerializeField] GameObject menu;
-    [SerializeField] GameObject options;
-    [SerializeField] GameObject top;
-    [SerializeField] GameObject game;
-    [SerializeField] GameObject result;
+    [SerializeField] GameObject converter;
 
     public static Action OnMousePressed { get; set; } = delegate { };
 
     private void Awake()
     {
+        navigation.SetActive(false);
         OpenWindow(0);
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape) && game.activeSelf)
-        {
-            OpenWindow(0);
-        }
     }
 
     public void OpenWindow(int windowIndex)
@@ -36,13 +30,12 @@ public class UIManager : MonoBehaviour
 
         switch(windowIndex)
         {
-            case 0: _last = menu; break;
-            case 1: _last = options; break;
-            case 2: _last = top; break;
-            case 3: _last = game; break;
-            case 4: _last = result; break;
+            case 0: _last = getStarted; break;
+            case 1: _last = menu; break;
+            case 2: _last = converter; break;
         }
 
+        navigation.SetActive(windowIndex > 0);
         _last.SetActive(true);
     }
 }
