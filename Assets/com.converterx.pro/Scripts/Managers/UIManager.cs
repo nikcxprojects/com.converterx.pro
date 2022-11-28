47c89771ca7899f4ca2a132a577f8790b522c517
Sladkiy Eduard <nikcxprojects@gmail.com>
2022-11-28 18:19:21 +0300

    update

--- Assets/com.converterx.pro/Scripts/Managers/UIManager.cs
+++ Assets/com.converterx.pro/Scripts/Managers/UIManager.cs
@@ -14,13 +14,18 @@ public class UIManager : MonoBehaviour
     [SerializeField] GameObject converter;
 
     [Space(10)]
+    [SerializeField] Dropdown inputCur;
+    [SerializeField] InputField enter;
+    [SerializeField] Dropdown outputCur;
+
+    [Space(10)]
     [SerializeField] Text resultText;
 
     public static Action OnMousePressed { get; set; } = delegate { };
 
     private void Awake()
     {
-        
+        resultText.text = string.Empty;
     }
 
     private void Start()
@@ -46,4 +51,26 @@ public class UIManager : MonoBehaviour
         navigation.SetActive(windowIndex > 0);
         _last.SetActive(true);
     }
+
+    public void Convert()
+    {
+        if (enter.text == string.Empty || inputCur.options[inputCur.value].text == "Choose input currency" || outputCur.options[outputCur.value].text == "Choose output currency")
+        {
+            return;
+        }
+
+        Format inputFormat = GetFormat(inputCur.options[inputCur.value].text);
+        Format outputFormat = GetFormat(outputCur.options[outputCur.value].text);
+
+        int value = int.Parse(enter.text);
+
+        resultText.text = $"RESULT:\n{value} {inputFormat}\n=\n{AppManger.Instance.GetResult(inputFormat, outputFormat, value)} {outputFormat}";
+    }
+
+    private Format GetFormat(string stringFormat) => stringFormat switch
+    {
+        "Русский Рубль(RUB)" => Format.RUB,
+        "US Dollar (USD)" => Format.USD,
+        "Euro (EUR)" => Format.EUR,
+    };
 }
